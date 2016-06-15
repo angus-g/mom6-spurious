5d959a2d4c38debaf579702a3b824126e75c32b9
Angus Gibson <angus.gibson@anu.edu.au>
2016-06-15 11:43:09 +1000

    [waves]: Correct initial condition
    The previous initial condition was incorrect, because it used a
    sinusoidal perturbation across the entire domain. This has been modified
    to use the perturbation only in the middle of the domain, setting up two
    internal waves travelling from the centre of the domain outwards.

--- config/internal_waves/mk_input.py
+++ config/internal_waves/mk_input.py
@@ -2,26 +2,30 @@ import numpy as np
 import xarray as xr
 
 # grid spacing
-dx = 5e3
+dx = 5
 # total grid size
-xl = 250e3
-NJ = 4
+xl = 250
+yl = 20
 dz = 25
 zl = 500
 
 t_bot = 10.1
 t_top = 20.1
 z_bot = 487.5
-L = 50e3
-x0 = 125e3
+L = 50
+x0 = 125
 A = 2
 
 ds = xr.Dataset(coords={'x': np.arange(dx / 2, xl, dx),
-                        'y': range(NJ),
+                        'y': np.arange(dx / 2, yl, dx),
                         'z': np.arange(dz / 2, zl, dz)})
 
-# force broadcasting into the y dimension
-ds['temp'] = t_bot + (t_top - t_bot)*(z_bot - ds.z) / z_bot + 0*ds.y - A*np.cos(np.pi / (2*L) * (ds.x - x0))*np.sin(np.pi*(ds.z + dz/2)/(z_bot + dz/2))
+# force broadcasting into the y and x dimensions
+ds['temp'] = t_bot + (t_top - t_bot)*(z_bot - ds.z) / z_bot + 0*ds.y + 0*ds.x
+# add anomaly in the region x0-L < x < x0+L
+ds['temp'] -= A*np.cos(np.pi / (2*L) * (ds.x - x0))*np.sin(np.pi*(ds.z + dz/2)/(z_bot + dz/2)) * ((x0 - L < ds.x) & (ds.x < x0 + L))
+
+# constant salinity
 ds['salt'] = 35 + 0*ds.z + 0*ds.y + 0*ds.x
 
 ds.to_netcdf('input.nc')

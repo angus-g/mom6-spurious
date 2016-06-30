29dd8e8fa9bef98e549955c67f8006e1a90cbe24
Angus Gibson <angus.gibson@anu.edu.au>
2016-06-30 10:47:59 +1000

    [analysis]: Allow RPE calculation to use topography
    Previously, the RPE calculation code assumes a flat-bottomed domain,
    without taking into account the effect of topography. It was assumed
    (based on some minimal evidence) that accounting for topography wouldn't
    significantly affect the calculated RPE, for the cost in the algorithm
    change. To allow for a more rigorous verification of this, it's possible
    to specify a 'topod' parameter for the 'calc_rpe' function.

--- analysis/lib.py
+++ analysis/lib.py
@@ -10,7 +10,7 @@ s_0 = 35.0
 # density change with temperature
 drho_dt = -0.2
 
-def calc_rpe(d, suffix='postale', step=None, var=None):
+def calc_rpe(d, suffix='postale', step=None, var=None, topod=None):
   """
   Calculate the RPE in dataset d. If var is None, the variables
   containing temperature and thickness data are given by 'T_' + suffix
@@ -19,6 +19,12 @@ def calc_rpe(d, suffix='postale', step=None, var=None):
   in the dictionary var. If step is None, the RPE is calculated at every
   timestep present in the dataset, otherwise it will be calculated at
   the specified frequency.
+
+  If topod is not None, it's expected to hold a Dataset containing
+  a variable 'depth', which gives the local depth of topography on
+  tracer points for the domain. The RPE calculation will then be
+  topography-aware, accounting for the contraction in domain area with
+  decreasing depth from the surface.
   """
 
   # assume a uniform grid-spacing and calculate the size
@@ -36,6 +42,20 @@ def calc_rpe(d, suffix='postale', step=None, var=None):
   else:
     t_all = d.variables[var['t']]
     h_all = d.variables[var['h']]
+
+  # extract some topography information
+  if topod is not None:
+    topo = topod.variables['depth'][:]
+    # construct a list of unique depth levels in the domain
+    depths, depth_counts = np.unique(topo, return_counts=True)
+    areas = depth_counts[::-1].cumsum()[::-1] * cell_area
+
+    # include the surface
+    depths = np.insert(depths, 0, 0)
+    areas = np.append(areas, 0)
+
+    # convert to heights above the deepest depth
+    depths = depths[-1] - depths
 
   # total number of timesteps
   nt = t_all.shape[0]
@@ -55,7 +75,38 @@ def calc_rpe(d, suffix='postale', step=None, var=None):
 
     indices = np.argsort(t, None)
     # reference interface positions
-    z_ref = np.insert(np.cumsum(h[indices] * cell_area / basin_area), 0, 0)
+    if topod is None:
+      z_ref = np.insert(np.cumsum(h[indices] * cell_area / basin_area), 0, 0)
+    else:
+      z_ref = np.empty(h.size + 1)
+      # calculate the volume of each cell
+      vs = h[indices] * cell_area
+
+      # bottom interface is at the very bottom, and integrate upwards
+      z_ref[0] = 0
+      cur = areas.size - 2
+
+      for j in range(1, z_ref.size):
+        z_ref[j] = z_ref[j-1]
+
+        # loop until we've accounted for all volume in this cell
+        while True:
+          # remaining volume in the current layer
+          vr = areas[cur] * (depths[cur] - z_ref[j])
+
+          if vr < 0:
+            print('negative volume! previous interface:', z_ref[j], depths[cur])
+
+          if vr > vs[j-1]:
+            # we can take all the volume of this cell out from the current layer
+            z_ref[j] += vs[j-1] / areas[cur]
+            break
+
+          # take volume out of this layer and continue
+          z_ref[j] = depths[cur]
+          vs[j-1] -= vr
+          cur -= 1
+
     # convert to layer positions
     z_lay = (z_ref[1:] + z_ref[:-1]) / 2.0
 
@@ -85,16 +136,18 @@ def diff2(u, x):
 
   return du
 
-def drpe_dt(f):
+def drpe_dt(f, ft=None):
   """
   Calculate the time rate of change for total
   RPE for a given netCDF file specified by f.
   """
 
   d = Dataset(f, 'r')
+  if ft is not None:
+    ft = Dataset(ft, 'r')
 
   # calculate RPE every timestep
-  rpe = calc_rpe(d)
+  rpe = calc_rpe(d, topod=ft)
   tv = d.variables['Time']
   time = tv[:]
   if 'minutes' in tv.units:
